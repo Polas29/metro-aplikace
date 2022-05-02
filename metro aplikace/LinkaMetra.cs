9ddd07cdb53d83bb4fe43d62e5908e63ad6279b2
adam.polacek <adam.polacek@VT1-08>
2022-05-02 13:17:27 +0200

    Přidejte soubory projektu.

---
+++ metro aplikace/LinkaMetra.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metro_aplikace
+{
+    internal class LinkaMetra
+    {
+        private List<string> _nazvyStanic;
+
+        public LinkaMetra(List<string> _vkladameStanice)
+        {
+           _nazvyStanic = _vkladameStanice;
+            Console.WriteLine("");
+        }
+        public void VypisStanice()
+        {
+            foreach (string stanice in _nazvyStanic) //projede všechny stanice v listu_nazvy stanic
+            {
+                Console.WriteLine(stanice); //vypisuje současnou stanici v cyklu
+            }
+        }
+        public void PrvniPosledníStanice()
+        {
+            string prvniStanice = _nazvyStanic[0];
+            string posledniStanice = _nazvyStanic[_nazvyStanic.Count - 1];
+            Console.WriteLine($"{prvniStanice} - {posledniStanice}");
+        }
+        public int ZjistiCas(string odjezdStanice, string prijezStanice)
+        {
+            int cas = 0;
+            if (_nazvyStanic.Contains(prijezStanice) && _nazvyStanic.Contains(odjezdStanice))
+            {
+                int indexOdjezdu = _nazvyStanic.IndexOf(odjezdStanice);
+                int indexPrijezdu = _nazvyStanic.IndexOf(prijezStanice);
+                int rozdil = indexOdjezdu - indexPrijezdu;
+                cas = rozdil * 2;
+            }
+            else
+            {
+                Console.WriteLine("co jeeeeeeeee");
+            }
+            return Math.Abs(cas);
+        }
+    }
+}
---
+++ metro aplikace/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using metro_aplikace;
+
+Console.WriteLine(@"  __  __ ______ _______ _____   ____  
+ |  \/  |  ____|__   __|  __ \ / __ \ 
+ | \  / | |__     | |  | |__) | |  | |
+ | |\/| |  __|    | |  |  _  /| |  | |
+ | |  | | |____   | |  | | \ \| |__| |
+ |_|  |_|______|  |_|  |_|  \_\\____/ ");
+List<string> LinkaA = new List<string>
+{
+    "Nemocnice Motol", "Petřiny", "Nádraží Veleslavín", "Bořislavka", "Dejvická", "Hradčanská",
+    "Malostranská" , "Staroměstská", "Muzeum", "Můstek", "Náměstí Míru", "Jiřího z Poděbrad",
+    "Flora", "Želivského", "STRAŠNICKÁ", "Skalka", "Depo Hostivař"
+};
+
+LinkaMetra linkaMetraA = new LinkaMetra(LinkaA);
+linkaMetraA.VypisStanice();
+linkaMetraA.PrvniPosledníStanice();
+Console.WriteLine("Zadej stanici odjezdu");
+string staniceA = Console.ReadLine();
+Console.WriteLine("Zadej stanici příjezdu");
+string staniceB = Console.ReadLine();
+
+int casCesty = linkaMetraA.ZjistiCas(staniceA, staniceB);
+Console.WriteLine(casCesty);
